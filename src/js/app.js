3fd35bc54b2842d5662f4c5dcb6333d5191a60bb
Natalia <nata@host>
2021-11-05 20:05:45 +0500

    проверка карт

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,26 @@ import valid from './validate';
 import Karts from './karts';
 const but = document.getElementById('submitform');
 but.addEventListener('click', () => {
+    ClearClass();
     let text = document.getElementById('card_number').value;
     console.log(text);
     console.log(valid(text));
     if (valid(text)) {
-        console.log(Karts(text));
-
+        // console.log(Karts(text));
+        const kart = document.querySelector(Karts(text));
+        kart.classList.add('map');
+        //console.log(kart);
     } else alert("Не верный номер карты!");
 });
+
+function ClearClass() {
+    const classCard = Array.from(document.getElementsByClassName('card'));
+    classCard.forEach(element => {
+        //console.log(element)
+        if (element.classList.contains('map')) {
+            element.classList.remove('map');
+        }
+
+    });
+
+}
