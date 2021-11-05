e959032761fde3dc226fd19fb4121019720f6b59
Natalia <nata@host>
2021-11-05 19:20:55 +0500

    проверка карт

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,13 @@
 // TODO: write your code here
 import valid from './validate';
+import Karts from './karts';
 const but = document.getElementById('submitform');
 but.addEventListener('click', () => {
     let text = document.getElementById('card_number').value;
-
+    console.log(text);
+    console.log(valid(text));
     if (valid(text)) {
+        console.log(Karts(text));
 
     } else alert("Не верный номер карты!");
 });
