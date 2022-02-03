b956f9ef20376d1d65225d59c2debd5653332384
Natlia82 <43696028+Natlia82@users.noreply.github.com>
2022-02-03 15:49:48 +0500

    lint пробелы

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,18 +1,19 @@
 export default function valid(value) {
-    if (/[^0-9-\s]+/.test(value)) return false;
-    // The Luhn Algorithm. It's so pretty.
-    let nCheck = 0;
-    let nDigit = 0;
-    let bEven = false;
-    const val = value.replace(/\D/g, '');
-    for (let n = val.length - 1; n >= 0; n -= 1) {
-        const cDigit = val.charAt(n);
-        nDigit = parseInt(cDigit, 10);
-        if (bEven) {
-            if ((nDigit = nDigit * 2) > 9) nDigit -= 9;
-        }
-        nCheck += nDigit;
-        bEven = !bEven;
+  if (/[^0-9-\s]+/.test(value)) return false;
+  // The Luhn Algorithm. It's so pretty.
+  let nCheck = 0;
+  let nDigit = 0;
+  let bEven = false;
+  const val = value.replace(/\D/g, '');
+  for (let n = val.length - 1; n >= 0; n -= 1) {
+    const cDigit = val.charAt(n);
+    nDigit = parseInt(cDigit, 10);
+    if (bEven) {
+      nDigit = nDigit * 2;
+      if ((nDigit) > 9) nDigit -= 9;
     }
-    return (nCheck % 10) === 0;
+    nCheck += nDigit;
+    bEven = !bEven;
+  }
+  return (nCheck % 10) === 0;
 }
